552723818fa05ce571386f1a464c6a9d1baaabc3
ipartzix <parthaPaul2705@gmail.com>
2026-02-28 11:09:31 +0530

    feat(Dynamic_Array): implement the resize , getitem function concept

--- 01_Arrays/Dynamic_Array.py
+++ 01_Arrays/Dynamic_Array.py
@@ -1,22 +1,43 @@
 import ctypes
+
 class MeraList:
     def __init__(self):
         self.size = 1
         self.n = 0
-        # create a C type array with size = self.size 
-        self.A = self.__make_array(self.size) # pyright: ignore[reportAttributeAccessIssue]
+        self.A = self.__make_array(self.size)
     
     def __len__(self):
         return self.n
     
-    def __make_array(self,capacity):
-        # this code create a c type array with fixed capacity also it is a referential array 
-        return (capacity*ctypes.py_object)()
+    def append(self, item):
+        if self.n == self.size:
+            # Resize when full
+            self.__resize(2 * self.size)
+        self.A[self.n] = item
+        self.n += 1
     
+    def __resize(self, new_capacity):
+        B = self.__make_array(new_capacity)
+        # Copy old elements
+        for i in range(self.n):
+            B[i] = self.A[i]
+        self.A = B
+        self.size = new_capacity
+    
+    def __getitem__(self, index):
+        if not 0 <= index < self.n:
+            raise IndexError("Index out of bounds")
+        return self.A[index]
+    
+    def __make_array(self, capacity):
+        return (capacity * ctypes.py_object)()
+
+# Usage
 L = MeraList()
-print(type(L))
-L =[10 ,20 ,30]
-print(len(L))
-L.append("Hello") # type: ignore
-print(L)
-print(len(L))
+L.append(10)
+L.append(20)
+L.append(30)
+print(len(L))   # Output: 3
+L.append("Hello")
+print(len(L))   # Output: 4
+print(L[3])     # Output: Hello
