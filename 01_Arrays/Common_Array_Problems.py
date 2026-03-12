4ea5dbcf322fd25d3b9e9a41fba6c12b9106adb0
ipartzix <parthaPaul2705@gmail.com>
2026-03-12 19:59:49 +0530

    feat(Array):add some code example about maximum sub array
    Signed-off-by: ipartzix <parthaPaul2705@gmail.com>

--- 01_Arrays/Common_Array_Problems.py
+++ 01_Arrays/Common_Array_Problems.py
@@ -47,3 +47,18 @@ def two_sum(arr, target):
 
 arr = [2, 7, 11, 15]
 print(two_sum(arr, 9))  # [0, 1] because arr[0] + arr[1] = 9
+
+print("Kadane’s Algorithm (Maximum Subarray Sum")
+def max_subarray_sum(arr):
+    max_sum = current_sum = arr[0]
+
+    for num in arr[1:]:
+        # Either extend current subarray or start fresh
+        current_sum = max(num, current_sum + num)
+        max_sum = max(max_sum, current_sum)
+
+    return max_sum
+
+
+arr = [-2, 1, -3, 4, -1, 2, 1, -5, 4]
+print(max_subarray_sum(arr))  # 6 (subarray [4,-1,2,1])
