4a88d057dee152e29a047565b9606fa2d1352a22
Partha Paul <parthapaul2705@gmail.com>
2026-04-04 22:07:14 +0530

    feat(Array):create array using array module

--- 01_Arrays/01_Basics_of_Array/create_array.py
+++ 01_Arrays/01_Basics_of_Array/create_array.py
@@ -1,9 +1,17 @@
-#Create an array and print all elements
+# #Create an array and print all elements
+# from array import array
+# import numpy as np
+# data_list: list[int] = [2, 4, 5, 67, 889, 9, 9, "hello ", True, 3.4, -12]
+# data_array: array = array('i', [1, 2, 3, 4, 5])
+# data_np: np.ndarray = np.array([1, 2, 3, 4, 5], dtype=np.int32)
+# print(data_array)
+# print(data_np)
+# print(data_list)
+# Create an array and print all elements
+
 from array import array
-import numpy as np
-data_list: list[int] = [2, 4, 5, 67, 889, 9, 9, "hello ", True, 3.4, -12]
-data_array: array = array('i', [1, 2, 3, 4, 5])
-data_np: np.ndarray = np.array([1, 2, 3, 4, 5], dtype=np.int32)
-print(data_array)
-print(data_np)
-print(data_list)
+
+arr = array('i', [2, 4, 5, 67, 889, 9])  # 'i' → integer array
+
+for element in arr:
+    print(element)
