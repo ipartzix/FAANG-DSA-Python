cbb8bb4b2212cfd7cc53d0fbe367ce41bc1c80bf
Partha Paul <parthapaul2705@gmail.com>
2026-04-05 22:01:14 +0530

    chore(Array):Count number of elements in array

---
+++ 01_Arrays/01_Basics_of_Array/count_of_elements.py
@@ -0,0 +1,8 @@
+# Count number of elements in array
+arr = [1, 2, 3, 4, 5,6,7,8,9,0]
+count = 0
+# Loop through each element in the array
+# '_' is used because we don't need the actual value
+for _ in arr:
+    count += 1
+print(count)
