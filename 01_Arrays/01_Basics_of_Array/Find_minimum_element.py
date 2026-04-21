95483d6f1c9b0c1a1ace9d1aadeedccfb2d70ea9
Partha Paul <parthapaul2705@gmail.com>
2026-04-21 21:09:37 +0530

    feat(Array):Find minimum element

---
+++ 01_Arrays/01_Basics_of_Array/Find_minimum_element.py
@@ -0,0 +1,12 @@
+# Find minimum element
+numbers = [3, 5, 1, 9, 2, 8]
+
+# Initialize the first element as the maximum
+min_element = numbers[0]
+
+# Iterate through the list to find the maximum
+for num in numbers:
+    if num < min_element:
+        min_element = num  # Update the maximum if a larger number is found
+
+print("Maximum element:", min_element)
