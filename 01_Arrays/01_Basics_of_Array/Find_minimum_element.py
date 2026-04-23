fa62020b363f7b93ecab28638913fd36d41b7b07
Partha Paul <parthapaul2705@gmail.com>
2026-04-23 10:13:26 +0530

    chore(Array): update thhe comment line and fix it as minimum

--- 01_Arrays/01_Basics_of_Array/Find_minimum_element.py
+++ 01_Arrays/01_Basics_of_Array/Find_minimum_element.py
@@ -1,12 +1,12 @@
 # Find minimum element
 numbers = [3, 5, 1, 9, 2, 8]
 
-# Initialize the first element as the maximum
+# Initialize the first element as the minimum
 min_element = numbers[0]
 
-# Iterate through the list to find the maximum
+# Iterate through the list to find the minimum
 for num in numbers:
     if num < min_element:
-        min_element = num  # Update the maximum if a larger number is found
+        min_element = num  # Update the minimum if a smaller number is found
 
 print("Maximum element:", min_element)
