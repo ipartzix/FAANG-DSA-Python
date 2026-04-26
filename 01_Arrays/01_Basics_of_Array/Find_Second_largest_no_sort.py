c8a48fcef24ad4a78ce23c8b5d7c7cca850dbbb9
Partha Paul <parthapaul2705@gmail.com>
2026-04-26 22:16:21 +0530

    chore(Array):find second largest in array

---
+++ 01_Arrays/01_Basics_of_Array/Find_Second_largest_no_sort.py
@@ -0,0 +1,21 @@
+# Find second largest element (without sorting)
+def second_largest(arr):
+    if len(arr) < 2:
+        return "Not enough elements"
+
+    first = float('-inf')   # largest
+    second = float('-inf')  # second largest
+
+    for x in arr:
+        if x > first:
+            second = first
+            first = x
+        elif x > second and x != first:
+            second = x
+
+    if second == float('-inf'):
+        return "No second largest"
+
+    return second
+
+
