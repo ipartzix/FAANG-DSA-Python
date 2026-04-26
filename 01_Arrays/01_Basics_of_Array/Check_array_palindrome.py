e24ce642413746d1ba57fa5b3d4e119c37185ccb
Partha Paul <parthapaul2705@gmail.com>
2026-04-26 20:59:31 +0530

    chore(Array): Check if array is palindrome

---
+++ 01_Arrays/01_Basics_of_Array/Check_array_palindrome.py
@@ -0,0 +1,20 @@
+# Check if array is palindrome
+def is_palindrome(arr):
+    left = 0
+    right = len(arr) - 1
+
+    while left < right:
+        if arr[left] != arr[right]:
+            return False
+        left += 1
+        right -= 1
+
+    return True
+
+
+# Example
+arr = [1, 2, 3, 2, 1]
+print(is_palindrome(arr))  # True
+
+arr2 = [1, 2, 3]
+print(is_palindrome(arr2))  # False
