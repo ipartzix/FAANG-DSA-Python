10990bdf558ede45d06846ed2bfb2d663822aec7
Partha Paul <parthapaul2705@gmail.com>
2026-04-28 19:22:54 +0530

    chore(Array):Print unique elements in array by using hash map (dictionary in Python)

--- 01_Arrays/01_Basics_of_Array/Print_unique_element.py
+++ 01_Arrays/01_Basics_of_Array/Print_unique_element.py
@@ -15,3 +15,26 @@ def print_unique(arr):
 # Example
 arr = [1, 2, 2, 3, 4, 4, 5]
 print_unique(arr)   # Output: 1 3 5
+
+
+#hash map (dictionary in Python)
+
+# def print_unique(arr):
+#     freq = {}
+#
+#     # Step 1: build frequency map
+#     for x in arr:
+#         if x in freq:
+#             freq[x] += 1
+#         else:
+#             freq[x] = 1
+#
+#     # Step 2: print elements with freq = 1
+#     for x in arr:
+#         if freq[x] == 1:
+#             print(x, end=" ")
+#
+#
+# # Example
+# arr = [1, 2, 2, 3, 4, 4, 5]
+# print_unique(arr)   # Output: 1 3 5
