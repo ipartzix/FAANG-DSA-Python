fec969a21243e0b95d52b2054cdf4fa9707172cc
Partha Paul <parthapaul2705@gmail.com>
2026-05-04 20:22:22 +0530

    fear(Arrayy): Count frequency of each element without dict first,

---
+++ 01_Arrays/01_Basics_of_Array/Count_frequency_of_each_element.py
@@ -0,0 +1,17 @@
+# Count frequency of each element (without dict first, then with dict)
+print("without dict ")
+arr = [2, 55, 67, 9, 74, 3, 89, 9, 55]
+
+visited = [False] * len(arr)
+
+for i in range(len(arr)):
+    if visited[i]:
+        continue
+
+    count = 1
+    for j in range(i + 1, len(arr)):
+        if arr[i] == arr[j]:
+            count += 1
+            visited[j] = True
+
+    print(arr[i], "->", count)
