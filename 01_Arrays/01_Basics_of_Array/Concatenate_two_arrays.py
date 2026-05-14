b3e197a9647590d3bcc883f949363f990bdfd3b5
Partha Paul <parthapaul2705@gmail.com>
2026-05-14 20:40:49 +0530

    fear(Array): Concatenate two arrays

---
+++ 01_Arrays/01_Basics_of_Array/Concatenate_two_arrays.py
@@ -0,0 +1,10 @@
+# Concatenate two arrays
+
+arr1 = [1, 2, 3]
+arr2 = [4, 5, 6]
+
+arr3 = arr1 + arr2
+
+print("First Array:", arr1)
+print("Second Array:", arr2)
+print("Concatenated Array:", arr3)
