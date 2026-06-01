328bcab95d0d31df4ce67e9b503a076c2030ddc9
Partha Paul <parthapaul2705@gmail.com>
2026-06-01 20:58:23 +0530

    chore(Array): update code structure

--- 01_Arrays/01_Basics_of_Array/user_input_array_reverse.py
+++ 01_Arrays/01_Basics_of_Array/user_input_array_reverse.py
@@ -1,10 +1,14 @@
 # Take array input from user and print in reverse
+
 def num():
     arr = list(map(int, input("Enter array elements (space separated): ").split()))
     # Take space-separated input, split into elements, convert each to integer, and store as a list (array)
     return arr
+
+
 #  User input → string → list of strings → integers → final array
 def reverse_array(arr):
+    
     left, right = 0, len(arr) - 1
     while left < right:
         arr[left], arr[right] = arr[right], arr[left]  # Swap
