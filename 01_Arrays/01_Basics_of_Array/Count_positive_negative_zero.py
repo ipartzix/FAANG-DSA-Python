141c459d37ad0b9d3aed2a34c7a02646e3a67013
Partha Paul <parthapaul2705@gmail.com>
2026-06-03 19:08:31 +0530

    chore(Array): update code structure

--- 01_Arrays/01_Basics_of_Array/Count_positive_negative_zero.py
+++ 01_Arrays/01_Basics_of_Array/Count_positive_negative_zero.py
@@ -8,10 +8,15 @@ zero_count = 0
 
 for num in numbers:
     if num > 0:
+
         positive_count += 1
+
     elif num < 0:
+
         negative_count += 1
+
     else:
+        
         zero_count += 1
 
 print("Positive numbers:", positive_count)
