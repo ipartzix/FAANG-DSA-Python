a9939e32334f6fa197c21c7ac92f49bed605a22f
Partha Paul <parthapaul2705@gmail.com>
2026-08-13 23:09:43 +0530

    feat(array): revision -Find the intersection of two arrays

---
+++ 00_Introduction/03_Array_Revision/14_Find_the_intersection_of_two_arrays.py
@@ -0,0 +1,11 @@
+# Find the intersection of two arrays
+arr1 = [1, 2, 3, 4]
+arr2 = [3, 4, 5, 6]
+
+intersection = []
+
+for x in arr1:
+    if x in arr2:
+        intersection.append(x)
+
+print(intersection)
